9821a69b08992f3c61ac20abca039cbde76ebe07
Emiliano <Emiliano>
2022-10-23 21:39:57 -0300

    cart

--- react38095/src/components/ItemDetailContainer.js
+++ react38095/src/components/ItemDetailContainer.js
@@ -3,29 +3,27 @@ import { useParams } from 'react-router-dom'
 
 import ItemDetail from '../components/ItemDetail'
 
+const ItemListContainer = ({ greeting }) => {
+  const [items, setItems] = useState([]);
+  const { categoryId } = useParams()
+
 useEffect(() => {
-  // crea una función que se va a encargar de traernos el producto
       const getProducto = async () => {
-  // creas una variable que traiga de la base de datos los productos que coincidan con el id del parámetro (useParams)
         const queryRef = doc(db, "products", id);
-  // guardarlos en una variable
         const response = await getDoc(queryRef);
-  // una vez que los tenés, crea un nuevo objeto con ese dato.
         const newItem = {
           id: response.id,
           ...response.data(),
         };
         setTimeout(()=>{
-  // y seteas el estado
           setItem(newItem);
         }, 2000)
       };
-  // llama a la función
       getProducto();
     }, [id]);
 
   return (
     <ItemDetail item={item}/>
   )
-
+  }
 export default ItemDetailContainer
--- react38095/src/context/CartContext.js
+++ react38095/src/context/CartContext.js
@@ -6,11 +6,29 @@ export const CartContext = createContext()
 export const CartProVider = ({Children}) => {
   const [cart, setCart]= useState([])
 
-const addToCart = (item) => {
+const addToCart = (item) => { 
+  const itemInCart = cart.find((prod)=> prod.id===item.id)
+if(itemInCart){
+  const updatedCart = cart.map((prod)=>{
+    if(prod.id === item.id){
+      return {...prod, quantity: prod.quantity + item.quantity}
+    }
+  })
+}else{
   setCart([...cart, item])
 }
+
+  
+}
+  const IsInCart = () => {
+    return  cart.some((prod) => prod.id === id)
+
+  }
+  
+
+
     return(
-        <CartContext.Provider value={{cart, addToCart}}>
+        <CartContext.Provider value={{cart, addToCart, Is}}>
         {Children}
         </CartContext.Provider>
     )
