8d0b13394931adb4cdba5149ba5295f8926cd0a2
Emiliano <Emiliano>
2022-10-28 00:01:57 -0300

    mas errores

--- react38095/src/components/ItemCard.js
+++ react38095/src/components/ItemCard.js
@@ -2,10 +2,10 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 
-const ItemCard = ( {id, name, price, img} ) => {
+const ItemCard = ( {id, name, price, pictureUrl} ) => {
   return (
     <Link to={`/item/${id}`}>
-      <img src={img} />
+      <img src={pictureUrl} />
       <div className='m-5'>
        <div>{id}</div>
         <div>{name}</div>
